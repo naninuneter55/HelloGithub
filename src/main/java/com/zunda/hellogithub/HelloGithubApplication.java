618a2d5e869cfe2be64c438e94f838f711844c62
Koji ONO <naninuneter55@gmail.com>
2016-04-18 19:56:07 +0900

    初期登録

---
+++ src/main/java/com/zunda/hellogithub/HelloGithubApplication.java
@@ -0,0 +1,12 @@
+package com.zunda.hellogithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGithubApplication.class, args);
+	}
+}
